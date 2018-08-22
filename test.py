5fdc6de2f8c4be0e832ee59bb34ed6e9e700022f
Rowan Lochrin <rowan.lochrin@gmail.com>
2018-08-22 16:44:35 -0700

    Added momentum,corrected summing of error values in backpropagation
    Added momentum to the weights. Also corrected the way that error is passed onto the next layer in backpropagation to correctly sum all the error from the neurons in the layer above.

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
 import random
 
 from data import Database
-from neural_network import Network, loss
+from nn_ import Network, loss
 
-nn = Network((4,5,5,7,3))
+nn = Network((4,5,3))
 
 print("Training: ")
 training_db = Database("iris_data_training.txt")
+
 num_epoch = 1000
+num_reports = 10
+n = num_epoch / num_reports
 
 for epoch in range(num_epoch):
     training_db.shuffle()
     results = list()
     for row in training_db.rows:
         row.output_vect = nn.forward(row.normalized)
-        nn.backpropagate(row.normalized, row.type_vect)
+        nn.backpropagate(row.normalized, row.output_vect, row.type_vect)
     total_loss = sum([loss(row.type_vect, row.output_vect) for row in training_db.rows])
-    print("Epoch: {:03}, Training Loss: {:.3f} ".format(epoch, total_loss))
+    if (epoch % n == 0):
+        print("Epoch: {:03}, Training Loss: {:.3f} ".format(epoch, total_loss))
 
 
 print("Testing: ")
